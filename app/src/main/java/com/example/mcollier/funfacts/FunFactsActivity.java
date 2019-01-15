78aabadcbff67a73e3771f3710fdc6feef9968d0
kencollier3 <mckennoncollier@gmail.com>
2019-01-15 09:06:22 -0700

    started adding variables

--- app/src/main/java/com/example/mcollier/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/example/mcollier/funfacts/FunFactsActivity.java
@@ -2,8 +2,13 @@ package com.example.mcollier.funfacts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class FunFactsActivity extends AppCompatActivity {
+    // Declare view variables
+    private TextView factTextView;
+    private Button showFactButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
